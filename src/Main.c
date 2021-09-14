d5363e93ba4502a6d575054894ae1f3326768a66
soniyasp20 <81178250+soniyasp20@users.noreply.github.com>
2021-09-14 22:38:29 +0530

    Update Main.c

--- src/Main.c
+++ src/Main.c
@@ -1,15 +1,15 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
-#include <A1Header.h>
-#include <A2Header.h>
-#include <A3Header.h>
-#include <A4Header.h>
+#include <Header_1.h>
+#include <Header_2.h>
+#include <Header_3.h>
+#include <Header_4.h>
 int main(void)
 {
     PortSet();
     ADC();
-    USARTInit(103);
+    USART(103);
     while(1)
     {
         Button_call();
