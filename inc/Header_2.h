b3d5ae977595329f42b2475277006fe56f51df03
soniyasp20 <81178250+soniyasp20@users.noreply.github.com>
2021-09-14 22:35:51 +0530

    Update Header_2.h

--- inc/Header_2.h
+++ inc/Header_2.h
@@ -10,7 +10,7 @@ void ADC();
 * Reads ADC output from a particular channel
 * Parameter : Channel number of data type uint8_t
 */
-uint16_t ReadADC(uint8_t ch);
+uint16_t ADCRead(uint8_t ch);
 
 #endif // A2HEADER_H_INCLUDED
 © 2021 GitHub, Inc.
