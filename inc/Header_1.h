fcfe0a21d3563e5642bd16d373bfc5ae67e5cf88
soniyasp20 <81178250+soniyasp20@users.noreply.github.com>
2021-09-14 22:27:05 +0530

    Add files via upload

---
+++ inc/Header_1.h
@@ -0,0 +1,14 @@
+#ifndef A1HEADER_H_INCLUDED
+#define A1HEADER_H_INCLUDED
+/* Brief
+* Sets the direction of the ports for the Buttons
+*/
+void PortSet();
+
+/* Brief
+* Gives output to LED if both the buttons are in 'close' position.
+*/
+void Buttons();
+
+
+#endif // A1HEADER_H_INCLUDED-
